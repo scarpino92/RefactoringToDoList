e84acbcea9e9c83f24e1fd28d3b9c0b07f7682ae
Serena Carpino <98754987+scarpino92@users.noreply.github.com>
2022-02-11 09:53:51 +0100

    Update todoLast.js

--- js/todoLast.js
+++ js/todoLast.js
@@ -1,6 +1,8 @@
+// ====================================================================================
+// versione funzionante definitiva
+// ====================================================================================
 import { Todo } from '../js/Todo.js';
 
-// versione funzionante definitiva
 $(() => {
     let todolist = [];
     
@@ -30,12 +32,11 @@ $(() => {
             todolist = JSON.parse(localLista); //trasforma in oggetto 
         }
 
-        // con aiuto esterno per la parte dell'each ------
         $.each(todolist, function (index, item) {
             $(lista).append(`<li class="list-group-item">${item.titolo} - ${item.testo} <span id='rimuovi' class='btn btn-sm btn-danger float-end'>X</span></li>`)
         });
         }
-        // ----------------------------------------------------
+       
         
         $(document).on('click', '#rimuovi', function () {
             let i = $(this.parentElement).index();
@@ -44,50 +45,11 @@ $(() => {
             stampaTodo();
         });
 
-
-    // stampaTodo();
-    // let button = $('.todo button');
-    // button.on('click', function () {
-    //     let titolo = $('#titolo').val();
-    //     let testo = $('#testo').val();
-    //     let todo = new Todo(titolo, testo);
-    //     if (titolo !== '' && testo !== '') {
-    //         todolist.push(todo);
-    //         stampaTodo();
-    //         localStorage.setItem('listaTodo', todolist);
-    //         $('#titolo').val('');
-    //         $('#testo').val('');
-    //     } else {
-    //         alert('Devi riempire i campi vuoti!');
-    //         return
-    //     }
-    // });
-    // function stampaTodo() {
-    //     let lista = $('.lista ul');
-    //     lista.html('');
-
-    //     let localLista = localStorage.getItem('listaTodo');
-    //     if (localLista !== null) {
-    //         todolist = JSON.parse(localLista);//trasforma in oggetto 
-    //     }
-
-    // ============================================================
-    //     $.each(todolist, function(i, todo){
-    //         let li = $('<li></li>').appendTo('.lista ul').addClass('list-group-item').html(todo.titolo + ' - ' + todo.testo).html('<span id="rimuovi" class="btn btn-sm btn-danger float-end">X</span>');
-    //         let rimuovi = $('#rimuovi');
-    //         rimuovi.on('click', function () {
-    //             todolist.splice(todolist.indexOf(todo), 1);
-    //             localStorage.setItem('listaTodo', JSON.stringify(todolist));
-    //             stampaTodo();
-    //         });
-    //     });
-    //}
-
     // =============================================================
-    // avevo provato anche sostituendo l'each con un ciclo for ...ma non andava...
+    // sostituendo l'each con un ciclo for ...
     // =============================================================
     // for (let i = 0; i < todolist.length; i++) {
-    //     lista += "<li class='list-group-item d-flex justify-content-between align-items-center'>" + todolist[i] + "<span id='rimuovi' onclick='rimuoviTask(" + i + ")' class='btn btn-sm btn-danger float-end'>X</span></li>";
+    //     lista += "<li class='list-group-item d-flex justify-content-between align-items-center'>" + todolist[i].titolo + ' - ' + todolist[i].testo + "<span id='rimuovi' onclick='rimuoviTask(" + i + ")' class='btn btn-sm btn-danger float-end'>X</span></li>";
     // }
     // $('.lista ul').html(lista);
 
@@ -103,7 +65,10 @@ $(() => {
     //     rimuoviTask(index);
     //     stampaTodo();
     // });
-// ===================================================================
+    
+    // ===================================================================
+    // file originale completamente in Js vanilla
+    // ===================================================================
     // function stampaTodo() {
     //     let lista = document.querySelector('.lista ul');
     //     lista.innerHTML = '';
